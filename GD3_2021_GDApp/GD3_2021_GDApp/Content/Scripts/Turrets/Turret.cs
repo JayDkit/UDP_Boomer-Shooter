083f4899efeba8f77a42cb8475eb4f1d379ef50b
Moustaicho <jay_150600@yahoo.fr>
2021-11-21 22:34:36 +0000

    Merged all branches
    BIG BUG: Models appear and disappear

---
+++ GD3_2021_GDApp/GD3_2021_GDApp/Content/Scripts/Turrets/Turret.cs
@@ -0,0 +1,31 @@
+﻿using GDLibrary;
+using GDLibrary.Components;
+using GDLibrary.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace GDApp.Content.Scripts.Turrets
+{
+    public class Turret : GameObject
+    {
+
+        public void InitializeModel(Scene level)
+        {
+            var material = new BasicMaterial("model material");
+            material.Texture = Application.Main.Content.Load<Texture2D>("Assets/Demo/Textures/grey");
+            material.Shader = new BasicShader(Application.Content);
+            var renderer = new ModelRenderer();
+            renderer.Material = material;
+            this.AddComponent(renderer);
+            renderer.Model = Application.Main.Content.Load<Model>("Assets/Models/cube");
+            //gun.Transform.SetScale(0.5f, 0.5f, 0.5f);
+            //gun.Transform.SetRotation(0, -90, 0);
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+}
